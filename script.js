9fbe988baacb109823d938940313375ff9321df9
krisskash <149309494+krisskash@users.noreply.github.com>
2024-01-19 18:10:50 +0200

    Small Adjustments

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkAvailabilityButton = document.getElementById('checkAvailabilityButton');
+    checkAvailabilityButton.addEventListener('click', function() {
+        console.log('Check Availability button clicked!');
+        // You can add more code here to handle the availability checking
+    });
+});
